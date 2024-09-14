7e26e172f8ad88354248b737857064c7229bdee2
MURAKAMI Masahiko <fossamagna2@gmail.com>
2024-09-14 17:52:41 +0900

    chore: enable Point-In-Time Recoverty

--- examples/sample-app/amplify/backend.ts
+++ examples/sample-app/amplify/backend.ts
@@ -1,11 +1,19 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data } from "./data/resource";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+const dataResources = backend.data.resources;
+
+Object.values(dataResources.cfnResources.amplifyDynamoDbTables).forEach(
+  (table) => {
+    table.pointInTimeRecoveryEnabled = true;
+  }
+);
