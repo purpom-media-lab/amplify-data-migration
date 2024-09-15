156829b9f7a3ce09f4ab161fd40aacf5d017ef92
MURAKAMI Masahiko <fossamagna2@gmail.com>
2024-09-16 00:10:57 +0900

    docs: translate public api docs into English (#7)

--- src/types/context.ts
+++ src/types/context.ts
@@ -2,13 +2,40 @@ import { AmplifyDynamoDBTable } from "../migration/types/dynamodb_table_provider
 import { DynamoDBExportKey } from "./dynamodb_export_key.js";
 import { ModelClient } from "./model_client.js";
 
+/**
+ * Context of migration processing.
+ */
 export interface MigrationContext {
-  tables: Record<string, AmplifyDynamoDBTable>; // key: modelName, value: dynamoDBTableName
-  exported: Record<string, DynamoDBExportKey>; // key: modelName, value: exported S3 key
+  /**
+   * A list of tables to be migrated.
+   * The key is the name of the model.
+   * The value is the table information.
+   */
+  tables: Record<string, AmplifyDynamoDBTable>;
+  /**
+   * A list of exported data.
+   * The key is the name of the model.
+   * The value is the key to the exported data.
+   */
+  exported: Record<string, DynamoDBExportKey>;
+  /**
+   * The client to be used for data migration.
+   */
   modelClient: ModelClient;
 }
 
+/**
+ * Context of export processing.
+ */
 export interface ExportContext {
-  tables: Record<string, AmplifyDynamoDBTable>; // key: modelName, value: dynamoDBTableArn
+  /**
+   * A list of tables to be migrated.
+   * The key is the name of the model.
+   * The value is the table information.
+   */
+  tables: Record<string, AmplifyDynamoDBTable>;
+  /**
+   * The client to be used for data migration.
+   */
   modelClient: ModelClient;
 }
